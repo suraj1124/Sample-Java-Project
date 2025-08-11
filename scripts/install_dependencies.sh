a02202edfa48361079815f983003d555404ceb13
suraj1124 <suraj1124>
2025-08-11 20:46:09 +0530

    added

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-mkdir -p /home/ec2-user/sample-java-project
-cd /home/ec2-user/sample-java-project
-sudo yum install -y nodejs                      # Installs both node and npm
-node -v                                         # Verify Node.js installation
-npm -v 
+sudo yum install -y nodejs
+node -v
+npm -v
