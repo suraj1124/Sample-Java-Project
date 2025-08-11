88f7facf8debac98bf26af06685bcf040f0b6dbe
suraj1124 <suraj1124>
2025-08-11 21:03:31 +0530

    added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = 'localhost';
+const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
