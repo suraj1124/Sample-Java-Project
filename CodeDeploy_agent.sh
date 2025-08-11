5378730cc4bf10c8a77dd7c1826c20c383e21f8e
suraj1124 <suraj1124>
2025-08-11 20:26:30 +0530

    added

---
+++ CodeDeploy_agent.sh
@@ -0,0 +1,6 @@
+sudo yum update -y
+sudo yum install ruby wget -y
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent status
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p /home/ec2-user/sample-java-project
+cd /home/ec2-user/sample-java-project
+npm install
